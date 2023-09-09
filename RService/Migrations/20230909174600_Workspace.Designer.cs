9aa4064cb5c6609be4139e45c7bc8e18251d9c93
Hamid Reza Mohammadi <mohammadi.hr@gmail.com>
2023-09-09 21:19:44 +0330

    db migration

--- RService/Migrations/20230909174600_Workspace.Designer.cs
+++ RService/Migrations/20230909174600_Workspace.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RService.DbContext;
 
@@ -11,9 +12,11 @@ using RService.DbContext;
 namespace RService.Migrations
 {
     [DbContext(typeof(RDbContext))]
-    partial class RDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230909174600_Workspace")]
+    partial class Workspace
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
